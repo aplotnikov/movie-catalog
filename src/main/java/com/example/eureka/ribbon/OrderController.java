e34c15f769d072694ab5491825d0d05c314ec8e0
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-22 15:31:14 +0100

    Fix typo

--- src/main/java/com/example/eureka/ribbon/OrderController.java
+++ src/main/java/com/example/eureka/ribbon/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.eureka.ribbon;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class OrderController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-    @RequestMapping(value = "/order", method = POST, produces = "application/json")
+    @RequestMapping(value = "/orders", method = POST, produces = "application/json")
     public Order orderMovie(@RequestBody NewOrderDetails newOrderDetails) {
         return new Order(UUID.randomUUID().toString());
     }

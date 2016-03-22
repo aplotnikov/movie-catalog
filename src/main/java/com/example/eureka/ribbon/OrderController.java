fb4bcdad5ba7a77a6808a91cba921f5767e851c8
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-22 15:14:12 +0100

    Replace Controller with RestController

--- src/main/java/com/example/eureka/ribbon/OrderController.java
+++ src/main/java/com/example/eureka/ribbon/OrderController.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@Controller
+@RestController
 public class OrderController {
 
     @RequestMapping(value = "/order", method = POST, produces = "application/json")
-    @ResponseBody
     public Order orderMovie(@RequestBody NewOrderDetails newOrderDetails) {
         return new Order(UUID.randomUUID().toString());
     }

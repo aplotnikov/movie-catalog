c5294d4efff2cdbfce0cbc29f32f664911069942
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-23 13:50:41 +0100

    Add logging

--- src/main/java/com/example/eureka/ribbon/OrderController.java
+++ src/main/java/com/example/eureka/ribbon/OrderController.java
@@ -1,11 +1,14 @@
 package com.example.eureka.ribbon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.invoke.MethodHandles;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,10 +18,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class OrderController {
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     private static AtomicInteger counter = new AtomicInteger();
 
     @RequestMapping(value = "/orders", method = POST, produces = "application/json")
     public ResponseEntity<Order> orderMovie(@RequestBody NewOrderDetails newOrderDetails) {
+        LOG.info("my message " + newOrderDetails.getTitle());
+
         if (counter.incrementAndGet() % 2 == 0) {
             LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(5));
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new Order("500"));

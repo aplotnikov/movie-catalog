32af939936a36e223205bb6449149f4f4606e2a1
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-03-23 11:42:36 +0100

    Add an ability to throw an exception

--- src/main/java/com/example/eureka/ribbon/OrderController.java
+++ src/main/java/com/example/eureka/ribbon/OrderController.java
@@ -1,21 +1,27 @@
 package com.example.eureka.ribbon;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class OrderController {
+    private static AtomicInteger counter = new AtomicInteger();
+
     @RequestMapping(value = "/orders", method = POST, produces = "application/json")
     public Order orderMovie(@RequestBody NewOrderDetails newOrderDetails) {
+        if (counter.incrementAndGet() % 2 == 0) {
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(5));
+            return new Order("500");
+        }
+
         return new Order(UUID.randomUUID().toString());
     }
 }
